1c6532ea2dddc97825b5182eb0003c49e36fe822
hanbyul0725 <s2234@e-mirim.hs.kr>
2024-08-16 11:38:02 +0900

    인덱서 실습

--- CSA031/Program.cs
+++ CSA031/Program.cs
@@ -3,3 +3,12 @@ static void Main(string[] args)
     Wanted<string> wantedString = new Wanted<string>("string");
     Wanted<int> wantedint = new Wanted<int>(52273);
     Wanted<double> wantedDouble = new Wanted<double>(52.273);
+
+    class Product
+{
+    public int this[int i]
+    {
+        get { return i; }
+        set { Console.WriteLine("{0}번째 상품 설정", i); }
+    }
+}
