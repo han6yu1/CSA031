85625428de8f76d03bfb2e09e68cf88f9e84832d
hanbyul0725 <s2234@e-mirim.hs.kr>
2024-08-16 11:08:10 +0900

    제네릭 실습1

---
+++ CSA031/Program.cs
@@ -0,0 +1,5 @@
+static void Main(string[] args)
+
+    Wanted<string> wantedString = new Wanted<string>("string");
+    Wanted<int> wantedint = new Wanted<int>(52273);
+    Wanted<double> wantedDouble = new Wanted<double>(52.273);
